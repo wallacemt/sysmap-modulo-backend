6b72a58bb267709637e38106ed98015d8f868422
Wallace Santana <wallacesantanak0@gmail.com>
2025-03-11 16:05:45 -0300

    (feat): Aula Introdução, Estrutura de dados

---
+++ aula1/index.js
@@ -0,0 +1,114 @@
+// <Constantes>
+// const a = "teste";
+// a = "teste2";
+
+//<escopo do var>
+// function teste() {
+//     if(true){
+//         var c = 'teste'
+//     }
+//     console.log(c);
+// }
+// teste();
+
+//<concatenação>
+// const numero = 999;
+
+// const a = "O número é " + numero;
+// const b = `O número é ${numero}`;
+
+// console.log(a);
+// console.log(b);
+
+//<tipagens>
+// const numero = 1231;
+// const str = "casa";
+// const bool = true;
+
+//<array>
+// const arr = [];
+
+// Não pode reatribuir
+//  arr = [];
+
+// Mas pode modificar o conteudo
+// arr.push(0);
+
+// console.log(arr);
+
+//<objetos>
+// const obj = {
+//   name: "joao",
+// };
+
+// obj.name = "maonel";
+// console.log(obj);
+
+// <spred>
+// const obj = {
+//     nome: 'joao',
+//     sobrenome: 'silva'
+// }
+
+// const user = {...obj, email:"joao@email.com"}
+
+// console.log(user);
+
+
+// <functions>
+
+// function sum(a, b){
+//     return a + b;
+// }
+
+// console.log(sum(1, 3));
+
+// <arrow function>
+// const somar = (a, b) => {
+//     return a + b;
+// }
+
+// console.log(somar(2, 2));
+
+// <condicionais>
+
+// const a = 0
+// if(a === 0){
+//     console.log("é 0")
+// }else if (a > 0){
+//     console.log('maior que 0')
+// }else{
+//     console.log('é menor que 0')
+// }
+
+// switch(a){
+//     case 0:
+//         console.log('é zero');
+//         break;
+//     case 1:
+//         console.log('maior que 0');
+//         break;
+//     default:
+//         console.log('menor que 0');
+//         break;
+// }
+
+//<repetições>
+const arr = [0, 1, 2, 3, 4];
+
+// 'for'
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// 'forEach' para cada elemento
+// arr.forEach((n) => {
+//     console.log(n)
+// })
+
+// 'while'
+// let i = 0;
+// while(i < arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
